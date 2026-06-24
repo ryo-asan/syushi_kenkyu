7a579091ec96ea4a0179372adbef7009c6fa5af9
ryo-asan <150892892+ryo-asan@users.noreply.github.com>
2026-06-24 11:58:36 +0900

    Initial commit

--- 卒論振り返り/BachelorFormatForLuaLaTeX/Bachelor/code/sample_program.cpp
+++ 卒論振り返り/BachelorFormatForLuaLaTeX/Bachelor/code/sample_program.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
+// #include <iostream>
 
-/**
- * @brief 要約説明
- * @param (引数名) 引数の説明
- * @return 戻り値の説明
- * @details 関数の細かい説明
- */
-void func() {
-	std::cout << "Welcome to the Sekine Lab!" << std::endl;
-}
+// /**
+//  * @brief 要約説明
+//  * @param (引数名) 引数の説明
+//  * @return 戻り値の説明
+//  * @details 関数の細かい説明
+//  */
+// void func() {
+// 	std::cout << "Welcome to the Sekine Lab!" << std::endl;
+// }
 
-int main() {
-	func();
+// int main() {
+// 	func();
 
-	return 0;
-}
+// 	return 0;
+// }
